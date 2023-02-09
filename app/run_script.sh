3b354f9fdcc124f24ed6fa563faa80fa6b203f4c
Joseph Szurley <jszurley@gmail.com>
2023-02-09 16:31:33 -0500

    Delete run_script.sh

--- app/run_script.sh
+++
@@ -1,4 +0,0 @@
-
-
-for nb_users in [1.e+00 1.e+01 1.e+02 1.e+03 1.e+04]:
-    //locust -f async.py --host "https://qualityestimation-staging-ng.unbabel.net" --run-time 300 --autostart -u 1 -r 1 --headless --csv test
